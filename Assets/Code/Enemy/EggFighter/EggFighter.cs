359c0df2914dbe8841690349b4ecb94776a9d106
sokilo <sokilopersonal@gmail.com>
2024-11-01 21:41:05 +0200

    Egg Fighter model, materials, textures and the class - SonicShader renamed to CommonSurge

---
+++ Assets/Code/Enemy/EggFighter/EggFighter.cs
@@ -0,0 +1,15 @@
+﻿using SurgeEngine.Code.StateMachine;
+using UnityEngine;
+
+namespace SurgeEngine.Code.Enemy
+{
+    public class EggFighter : MonoBehaviour
+    {
+        private FStateMachine _stateMachine;
+        
+        private void Awake()
+        {
+            _stateMachine = new FStateMachine();
+        }
+    }
+}
