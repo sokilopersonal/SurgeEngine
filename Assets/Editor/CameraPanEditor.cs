808dc5b4aea858c941c8e350f993e9dd6d48a09c
loco <61364136+locogit@users.noreply.github.com>
2024-12-21 16:28:56 -0500

    start of camera pan inspector

---
+++ Assets/Editor/CameraPanEditor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(SurgeEngine.Code.CommonObjects.ObjCameraPan))]
+public class CameraPanEditor : Editor
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
