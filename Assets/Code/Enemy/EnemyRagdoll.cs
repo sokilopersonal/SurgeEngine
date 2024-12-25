171d4391dc476c557eaed5039bac89df7b147301
loco <61364136+locogit@users.noreply.github.com>
2024-12-24 19:04:18 -0500

    Ragdolls explode when hitting enemies

--- Assets/Code/Enemy/EnemyRagdoll.cs
+++ Assets/Code/Enemy/EnemyRagdoll.cs
@@ -40,5 +40,20 @@ namespace SurgeEngine.Code.Enemy
 
             Destroy(gameObject);
         }
+
+        public void ExplodeImmediate()
+        {
+            if (hit)
+                return;
+
+            hit = true;
+
+            ParticleSystem particle = Instantiate(explosionEffect, root.position + Vector3.up * explosionOffset, Quaternion.identity);
+            Destroy(particle.gameObject, 2.5f);
+
+            RuntimeManager.PlayOneShot(explosionReference, root.position);
+
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Code/Enemy/EnemyRagdollLimb.cs
+++ Assets/Code/Enemy/EnemyRagdollLimb.cs
@@ -5,9 +5,16 @@ namespace SurgeEngine.Code.Enemy
     public class EnemyRagdollLimb : MonoBehaviour
     {
         public EnemyRagdoll ragdoll;
+        int enemyLayer = 69;
+        private void Awake()
+        {
+            enemyLayer = LayerMask.NameToLayer("Enemy");
+        }
         public void OnCollisionEnter(Collision collision)
         {
-            if (ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
+            if (collision.gameObject.layer == enemyLayer)
+                ragdoll.ExplodeImmediate();
+            else if (ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
                 ragdoll.Explode();
         }
     }
