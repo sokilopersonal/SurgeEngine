ea83a8e88e4ea18f2ef716df79d3e510ef19f70c
loco <61364136+locogit@users.noreply.github.com>
2024-12-27 16:43:01 -0500

    Fix Ragdoll

--- Assets/Code/Enemy/EnemyRagdollLimb.cs
+++ Assets/Code/Enemy/EnemyRagdollLimb.cs
@@ -5,19 +5,11 @@ namespace SurgeEngine.Code.Enemy
     public class EnemyRagdollLimb : MonoBehaviour
     {
         public EnemyRagdoll ragdoll;
-        int enemyLayer = 69;
-        private void Awake()
-        {
-            enemyLayer = LayerMask.NameToLayer("Enemy");
-        }
         public void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.layer == enemyLayer)
-                ragdoll.ExplodeImmediate();
-            else if (ragdoll.timer >= ragdoll.minimumLifeTime && ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
+            if (ragdoll.timer >= ragdoll.minimumLifeTime && ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
                 ragdoll.Explode();
         }
-
         public void OnCollisionStay(Collision collision)
         {
             if (ragdoll.timer < ragdoll.minimumLifeTime)
