9df053f5a66a2d0fddf828dd666e4b3587e220e7
loco <61364136+locogit@users.noreply.github.com>
2024-12-29 22:21:07 -0500

    Ragdoll Tweaks

--- Assets/Code/Enemy/EggFighter/States/EGStateDead.cs
+++ Assets/Code/Enemy/EggFighter/States/EGStateDead.cs
@@ -7,8 +7,6 @@ namespace SurgeEngine.Code.Enemy.States
     public class EGStateDead : EGState
     {
         private float _timer;
-        private bool ragdollCreated = false;
-
         public EGStateDead(EggFighter eggFighter, Transform transform, Rigidbody rb) : base(eggFighter, transform, rb)
         {
 
@@ -23,27 +21,12 @@ namespace SurgeEngine.Code.Enemy.States
             {
                 context.stateMachine.SetState<FStateAfterHoming>();
             }
-
-            Object.Destroy(eggFighter.gameObject);
         }
 
         public void ApplyKnockback(Vector3 force, EnemyRagdoll ragdoll)
         {
-            if (ragdollCreated)
-                return;
-
-            ragdollCreated = true;
-
-            GameObject newRagdoll = Object.Instantiate(ragdoll.gameObject, transform.parent);
-            newRagdoll.transform.position = transform.position;
-            newRagdoll.transform.forward = -ActorContext.Context.transform.forward;
-
-            EnemyRagdoll newRagdollScript = newRagdoll.GetComponent<EnemyRagdoll>();
-
-            Object.Destroy(eggFighter.gameObject);
-
-            newRagdollScript.root.AddForce(force * 3f, ForceMode.VelocityChange);
-            newRagdollScript.root.AddTorque(force, ForceMode.VelocityChange);
+            ragdoll.Ragdoll(force);
+            eggFighter.animation.animator.enabled = false;
         }
     }
 }
--- Assets/Code/Enemy/EnemyRagdollLimb.cs
+++ Assets/Code/Enemy/EnemyRagdollLimb.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace SurgeEngine.Code.Enemy
 {
+    [RequireComponent(typeof(Collider), typeof(Rigidbody))]
     public class EnemyRagdollLimb : MonoBehaviour
     {
+        [HideInInspector]
         public EnemyRagdoll ragdoll;
+
+        private Collider col;
+        private Rigidbody rb;
+        private bool active = false;
+        private void Awake()
+        {
+            col = GetComponent<Collider>();
+            rb = GetComponent<Rigidbody>();
+            SetActive(active);
+        }
+        public void SetActive(bool _set)
+        {
+            active = _set;
+            col.enabled = active;
+            rb.isKinematic = !active;
+        }
+        public void AddForce(Vector3 force, ForceMode mode)
+        {
+            if (!active)
+                return;
+            rb.AddForce(force, mode);
+        }
         public void OnCollisionEnter(Collision collision)
         {
-            if (ragdoll.timer >= ragdoll.minimumLifeTime && ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
+            if (active && ragdoll != null && ragdoll.timer > ragdoll.minimumLifeTime && ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
                 ragdoll.Explode();
         }
         public void OnCollisionStay(Collision collision)
         {
-            if (ragdoll.timer < ragdoll.minimumLifeTime)
-                return;
-
-            if (ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
+            if (active && ragdoll != null && ragdoll.timer > ragdoll.minimumLifeTime && ragdoll.collideLayers == (ragdoll.collideLayers | (1 << collision.gameObject.layer)))
                 ragdoll.Explode();
         }
     }
