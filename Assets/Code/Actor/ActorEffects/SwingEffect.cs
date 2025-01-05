2112a52651020388aeb12e1b139dccc3616970aa
loco <61364136+locogit@users.noreply.github.com>
2025-01-05 13:11:56 -0500

    2D Swing Trail

--- Assets/Code/Actor/ActorEffects/SwingEffect.cs
+++ Assets/Code/Actor/ActorEffects/SwingEffect.cs
@@ -9,8 +9,10 @@ namespace SurgeEngine.Code.ActorEffects
     public class SwingEffect : Effect
     {
         public TrailRenderer trail;
+        public bool trail2D = false;
         public override void Toggle(bool value)
         {
+            trail.transform.localEulerAngles = new Vector3(trail2D ? 90 : 0, 0, 0);
             trail.emitting = value;
         }
     }
