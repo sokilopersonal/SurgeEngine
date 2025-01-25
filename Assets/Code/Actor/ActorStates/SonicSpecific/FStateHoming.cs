70f5fcf2e93e7e069c976858bc0a63e54c0f45ea
sokilo <sokilopersonal@gmail.com>
2025-01-25 13:21:22 +0200

    Decreased homing damage box size

--- Assets/Code/Actor/ActorStates/SonicSpecific/FStateHoming.cs
+++ Assets/Code/Actor/ActorStates/SonicSpecific/FStateHoming.cs
@@ -90,7 +90,7 @@ namespace SurgeEngine.Code.ActorStates.SonicSpecific
                 }
             }
             
-            if (HurtBox.CreateAttached(Actor, Actor.transform, Vector3.one))
+            if (HurtBox.CreateAttached(Actor, Actor.transform, Vector3.one * 0.5f))
             {
                 StateMachine.SetState<FStateAfterHoming>();
             }
