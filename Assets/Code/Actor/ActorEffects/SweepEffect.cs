f3972df0c065d9d582658ec285108d04f216ec39
loco <61364136+locogit@users.noreply.github.com>
2025-01-28 19:14:30 -0500

    Better sweepkick

---
+++ Assets/Code/Actor/ActorEffects/SweepEffect.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace SurgeEngine.Code.ActorEffects
+{
+    public class SweepEffect : Effect
+    {
+        [SerializeField] private Light pointLight;
+        private float startIntensity;
+
+        private void Start()
+        {
+            startIntensity = pointLight.intensity;
+        }
+
+        public override void Toggle(bool value)
+        {
+            if (value)
+            {
+                pointLight.intensity = 0;
+                pointLight.enabled = true;
+                pointLight.DOKill();
+                pointLight.DOIntensity(startIntensity, 0.5f).SetDelay(0.1f).OnComplete(() =>
+                {
+                    pointLight.DOIntensity(0f, 0.5f).OnComplete(() =>
+                    {
+                        pointLight.enabled = false;
+                    });
+                });
+            }
+            else
+            {
+                pointLight.DOIntensity(0, 0.5f).OnComplete(() =>
+                {
+                    pointLight.enabled = false;
+                });
+            }
+            base.Toggle(value);
+        }
+
+        public override void Clear()
+        {
+            pointLight.enabled = false;
+            pointLight.intensity = 0f;
+            base.Clear();
+        }
+    }
+}
