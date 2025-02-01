407dc172369b0a49361b1025d8ca85a180c28022
sokilo <sokilopersonal@gmail.com>
2025-02-01 11:18:17 +0200

    Added scene validator

---
+++ Assets/Editor/SceneValidator.cs
@@ -0,0 +1,48 @@
+﻿using SurgeEngine.Code.CommonObjects;
+using UnityEditor;
+using UnityEngine;
+
+namespace SurgeEngine.Editor
+{
+    public static class SceneValidator
+    {
+        [MenuItem("Surge Engine/Validate Scene")]
+        public static void ValidateScene()
+        {
+            string msg = "It's better to validate a scene for better navigation. This action will add the necessary folders to the scene." +
+                         " \nAre you sure you want to continue?";
+            if (EditorUtility.DisplayDialog("Scene Validation", msg, "Yes", "No"))
+            {
+                if (!Object.FindFirstObjectByType<Stage>())
+                {
+                    var stage = new GameObject("Stage").AddComponent<Stage>();
+                    Undo.RegisterCreatedObjectUndo(stage, "Add Stage");
+                }
+
+                if (!GameObject.Find("- Volumes"))
+                {
+                    var volumes = new GameObject("- Volumes");
+                    Undo.RegisterCreatedObjectUndo(volumes, "Add Volumes folder");
+                }
+
+                if (!GameObject.Find("- SetData"))
+                {
+                    var setdata = new GameObject("- SetData");
+                    Undo.RegisterCreatedObjectUndo(setdata, "Add SetData folder");
+                }
+
+                if (!GameObject.Find("- Geometry"))
+                {
+                    var geometry = new GameObject("- Geometry");
+                    Undo.RegisterCreatedObjectUndo(geometry, "Add Geometry folder");
+                }
+
+                if (!GameObject.Find("- Lighting"))
+                {
+                    var lighting = new GameObject("- Lighting");
+                    Undo.RegisterCreatedObjectUndo(lighting, "Add Lighting folder");
+                }
+            }
+        }
+    }
+}
