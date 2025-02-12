a0e00c9db80e53eb49563582b133e29c9748c2b8
sokilo <sokilopersonal@gmail.com>
2025-02-12 20:44:57 +0200

    Foot IK
    # Conflicts:
    #	Assets/Scenes/NewTestStage.unity
    #	ProjectSettings/Packages/com.unity.probuilder/Settings.json

---
+++ Assets/Code/Actor/ActorSystem/IK/ActorIKController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SurgeEngine.Code.StateMachine.Components;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+namespace SurgeEngine.Code.ActorSystem.IK
+{
+    public class ActorIKController : MonoBehaviour
+    {
+        [SerializeField] private Rig[] rigs;
+        [SerializeField] private float weightSpeed = 14;
+        private StateAnimator _stateAnimator;
+
+        private void Awake()
+        {
+            _stateAnimator = GetComponent<StateAnimator>();
+
+            foreach (var rig in rigs)
+            {
+                rig.weight = 1;
+            }
+        }
+
+        private void Update()
+        {
+            float value = _stateAnimator.IsIKAllowed() ? 1 : 0;
+            foreach (var rig in rigs)
+            {
+                rig.weight = Mathf.Lerp(rig.weight, value, Time.deltaTime * weightSpeed);
+            }
+        }
+    }
+}
