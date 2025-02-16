6c838f9a4a8a7d1abe19c185bcf19093103c5fc1
sokilo <sokilopersonal@gmail.com>
2025-02-17 00:59:34 +0200

    Added selection box cool ui particles

---
+++ Assets/Code/UI/SelectionBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace SurgeEngine.Code.UI
+{
+    public class SelectionBox : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rect;
+
+        private void Awake()
+        {
+            Vector2 delta = rect.sizeDelta;
+            delta.y = 0;
+            rect.sizeDelta = delta;
+        }
+    }
+}
--- Assets/Code/UI/UnselectOnHover.cs
+++ Assets/Code/UI/UnselectOnHover.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace SurgeEngine.Code.UI
 {
