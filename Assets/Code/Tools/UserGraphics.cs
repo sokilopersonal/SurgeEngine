d2a57511aa53338c15aa02cfd6eb957456f71c11
sokilo <sokilopersonal@gmail.com>
2025-04-10 12:45:10 +0300

    Refraction quality keyword

--- Assets/Code/Tools/UserGraphics.cs
+++ Assets/Code/Tools/UserGraphics.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.HighDefinition;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace SurgeEngine.Code.Tools
 {
@@ -16,6 +18,13 @@ namespace SurgeEngine.Code.Tools
         private HDAdditionalLightData _sunData;
         private readonly List<Light> _additionalLights;
         private readonly List<HDAdditionalLightData> _additionalLightsData;
+
+        private readonly string[] _refractionQualityKeywords =
+        {
+            "_REFRACTIONQUALITY_NATIVE",
+            "_REFRACTIONQUALITY_MEDIUM", 
+            "_REFRACTIONQUALITY_LOW"
+        };
 
         public UserGraphics(VolumeProfile profile)
         {
@@ -98,6 +107,11 @@ namespace SurgeEngine.Code.Tools
             }
         }
 
+        public void SetRefractionQuality(RefractionQuality level)
+        {
+            SetKeyword(_refractionQualityKeywords, (int)level);
+        }
+
         private void SetSun(Light sun)
         {
             if (sun.type == LightType.Directional)
@@ -133,6 +147,7 @@ namespace SurgeEngine.Code.Tools
                 SetTextureQuality(0);
                 SetSunShadowsQuality(-1);
                 SetAdditionalShadowsQuality(-1);
+                SetRefractionQuality(RefractionQuality.Low);
             }
             
             if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -140,6 +155,7 @@ namespace SurgeEngine.Code.Tools
                 SetTextureQuality(1);
                 SetSunShadowsQuality(0);
                 SetAdditionalShadowsQuality(0);
+                SetRefractionQuality(RefractionQuality.Medium);
             }
             
             if (Input.GetKeyDown(KeyCode.Alpha3))
@@ -147,6 +163,7 @@ namespace SurgeEngine.Code.Tools
                 SetTextureQuality(2);
                 SetSunShadowsQuality(1);
                 SetAdditionalShadowsQuality(1);
+                SetRefractionQuality(RefractionQuality.Native);
             }
             
             if (Input.GetKeyDown(KeyCode.Alpha4))
@@ -199,6 +216,17 @@ namespace SurgeEngine.Code.Tools
                 SetTextureQuality(data.TextureQuality);
             }
         }
+
+        private static void SetKeyword(string[] keys, int value)
+        {
+            string key = keys[value];
+            foreach (var keyword in keys)
+            {
+                Shader.DisableKeyword(keyword);
+            }
+            
+            Shader.EnableKeyword(key);
+        }
     }
 
     public class GraphicsData
@@ -209,5 +237,12 @@ namespace SurgeEngine.Code.Tools
         public int AoQuality;
         public int MotionBlurQuality;
         public int TextureQuality;
+    }
+    
+    public enum RefractionQuality
+    {
+        Native,
+        Medium,
+        Low
     }
 }
