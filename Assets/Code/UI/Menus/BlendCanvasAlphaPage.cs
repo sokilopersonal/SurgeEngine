3b8d5278cd659afe540a0e9f11057c33bbef16ae
sokilo <sokilopersonal@gmail.com>
2025-04-14 08:58:10 +0300

    Blend Canvas Alpha Page

---
+++ Assets/Code/UI/Menus/BlendCanvasAlphaPage.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace SurgeEngine.Code.UI.Menus
+{
+    public class BlendCanvasAlphaPage : Page
+    {
+        [SerializeField] private Ease easing;
+
+        protected override void InsertIntroAnimations()
+        {
+            AnimationSequence.Join(Group.DOFade(1f, duration)).SetEase(easing);
+        }
+
+        protected override void InsertOutroAnimations()
+        {
+            AnimationSequence.Join(Group.DOFade(0f, duration)).SetEase(easing);
+        }
+    }
+}
