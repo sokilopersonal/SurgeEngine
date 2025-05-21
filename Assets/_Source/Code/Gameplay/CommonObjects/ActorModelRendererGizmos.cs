3adadc051929a6cb162324b6b6722cbdc6577891
sokilo <sokilopersonal@gmail.com>
2025-05-21 17:12:56 +0300

    Added ActorModelRendererGizmos to render actor model in editor - Made a Sonic renderer preset - Removed SpawnPoint tag, you have to assign spawn point transform in the ActorInstaller

---
+++ Assets/_Source/Code/Gameplay/CommonObjects/ActorModelRendererGizmos.cs
@@ -0,0 +1,41 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace SurgeEngine._Source.Code.Gameplay.CommonObjects
+{
+    [ExecuteInEditMode]
+    public class ActorModelRenderer : MonoBehaviour
+    {
+        [Space(10)]
+        [InfoBox("Assign the mesh and materials per sub mesh. Project contains an editor preset with Sonic mesh and materials configured.")]
+        [Space(10)]
+        [SerializeField] private Mesh meshToRender;
+        [SerializeField] private Material[] m_Materials; // Copying doesn't work :(
+        
+        private void Update()
+        {
+            if (Application.isPlaying || !Application.isEditor)
+                return;
+            
+            if (meshToRender == null || m_Materials == null || m_Materials.Length == 0)
+            {
+                return;
+            }
+
+            int submeshCount = meshToRender.subMeshCount;
+            Matrix4x4 matrix = Matrix4x4.TRS(transform.position + Vector3.down, transform.rotation, transform.lossyScale);
+
+            for (int subMeshIndex = 0; subMeshIndex < submeshCount; subMeshIndex++)
+            {
+                if (subMeshIndex < m_Materials.Length && m_Materials[subMeshIndex] != null)
+                {
+                    m_Materials[subMeshIndex].SetPass(0);
+                    Graphics.DrawMesh(meshToRender, matrix, m_Materials[subMeshIndex], 0, null, subMeshIndex, null, 
+                        true, 
+                        true, 
+                        true);
+                }
+            }
+        }
+    }
+}
--- Assets/_Source/Code/Infrastructure/DI/ActorInstaller.cs
+++ Assets/_Source/Code/Infrastructure/DI/ActorInstaller.cs
@@ -1,18 +1,33 @@
+using System;
+using NaughtyAttributes;
 using SurgeEngine.Code.Core.Actor.States;
 using SurgeEngine.Code.Core.Actor.System;
 using UnityEngine;
 using Zenject;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace SurgeEngine.Code.Infrastructure.DI
 {
     public class ActorInstaller : MonoInstaller
     {
-        [SerializeField] private Transform actorPrefab;
+        [SerializeField, Required] private Transform actorPrefab;
+        [SerializeField, Required] private Transform spawnPoint;
         [SerializeField] private StartData data;
 
         public override void InstallBindings()
         {
-            data.StartTransform = GameObject.FindGameObjectWithTag("SpawnPoint").transform;
+            if (!spawnPoint)
+            {
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+                throw new NullReferenceException("Spawn Point is not assigned, please do it in ActorInstaller under GameplaySceneContext. Stopping play mode...");
+#endif
+            }
+            
+            data.StartTransform = spawnPoint;
 
             var instance = Container.InstantiatePrefabForComponent<ActorBase>(actorPrefab, data.StartTransform.position, data.StartTransform.rotation, null);
 
@@ -24,12 +39,6 @@ namespace SurgeEngine.Code.Infrastructure.DI
             Quaternion par = instance.transform.parent.rotation;
             instance.transform.parent.rotation = Quaternion.identity;
             instance.transform.rotation = par;
-            
-            var startRenderer = data.StartTransform.GetComponentInChildren<Renderer>();
-            if (startRenderer)
-            {
-                startRenderer.enabled = false; // Disable model if exists after spawning
-            }
         }
     }
 }
