bdca380fc3683ebb1446a92b88c73593e9e4137c
sokilo <sokilopersonal@gmail.com>
2025-06-27 15:16:24 +0300

    TryGetActor in the water surface

--- Assets/_Source/Code/Gameplay/CommonObjects/Environment/WaterSurface.cs
+++ Assets/_Source/Code/Gameplay/CommonObjects/Environment/WaterSurface.cs
@@ -155,7 +155,7 @@ namespace SurgeEngine.Code.Gameplay.CommonObjects.Environment
         {
             if (_surfaceRigidbody)
             {
-                if (other.transform.TryGetComponentInParent(out ActorBase actor))
+                if (_surfaceRigidbody.TryGetActor(out ActorBase _))
                 {
                     Vector3 splashPoint = _surfaceRigidbody.position;
                     splashPoint.y -= 0.75f;
