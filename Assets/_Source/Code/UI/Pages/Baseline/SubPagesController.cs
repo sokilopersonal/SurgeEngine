d533155f0fa011d07b1c1cffcc9430203c6d61af
sokilo <sokilopersonal@gmail.com>
2025-06-29 13:26:22 +0300

    Do not enter the same sub page

--- Assets/_Source/Code/UI/Pages/Baseline/SubPagesController.cs
+++ Assets/_Source/Code/UI/Pages/Baseline/SubPagesController.cs
@@ -7,14 +7,20 @@ namespace SurgeEngine.Code.UI.Pages.Baseline
     {
         [SerializeField] private Page[] subPages;
 
+        private Page _current;
+        
         public void Push(Page page)
         {
+            if (_current == page)
+                return;
+            
             foreach (var subPage in subPages)
             {
                 subPage.Exit();
             }
             
             page.Enter();
+            _current = page;
         }
     }
 }
