6fd3172905e3d3aa778c178aaa6b8d3ebc561962
sokilo <sokilopersonal@gmail.com>
2025-07-13 23:31:48 +0300

    wtf

---
+++ Assets/_Source/Code/Gameplay/CommonObjects/Mobility/JumpPanelBase.cs
@@ -0,0 +1,37 @@
+﻿using SurgeEngine.Code.Core.Actor.States;
+using SurgeEngine.Code.Core.Actor.States.Characters.Sonic.SubStates;
+using SurgeEngine.Code.Core.Actor.System;
+using SurgeEngine.Code.Infrastructure.Custom;
+using SurgeEngine.Code.Infrastructure.Tools;
+using UnityEngine;
+
+namespace SurgeEngine.Code.Gameplay.CommonObjects.Mobility
+{
+    public abstract class JumpPanelBase : ContactBase
+    {
+        [SerializeField] protected float impulse = 30;
+        [SerializeField] protected float outOfControl = 0.5f;
+        
+        protected void Launch(ActorBase context, float pitch)
+        {
+            if (impulse > 0)
+            {
+                bool boosted = SonicTools.IsBoost();
+                if (boosted)
+                    context.Effects.JumpDeluxEffect.Toggle(true);
+
+                context.StateMachine.GetSubState<FBoost>().Active = false;
+                
+                context.transform.forward = Vector3.Cross(-transform.right, Vector3.up);
+                context.Kinematics.Rigidbody.linearVelocity = Utility.GetImpulseWithPitch(-transform.forward, transform.right, pitch, impulse);
+
+                var jumpPanelState = context.StateMachine.GetState<FStateJumpPanel>();
+                jumpPanelState.SetDelux(boosted);
+                jumpPanelState.SetKeepVelocity(outOfControl);
+                context.StateMachine.SetState<FStateJumpPanel>(0f, true, true);
+                    
+                context.Flags.AddFlag(new Flag(FlagType.OutOfControl, true, Mathf.Abs(outOfControl)));
+            }
+        }
+    }
+}
