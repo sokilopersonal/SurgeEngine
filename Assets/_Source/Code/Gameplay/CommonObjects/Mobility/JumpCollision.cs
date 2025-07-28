b257107f6d87f048c4dc5a13e64a8fbefce65794
sokilo <sokilopersonal@gmail.com>
2025-07-28 12:02:57 +0300

    Jump collision update, ground only parameter, pitch

--- Assets/_Source/Code/Gameplay/CommonObjects/Mobility/JumpCollision.cs
+++ Assets/_Source/Code/Gameplay/CommonObjects/Mobility/JumpCollision.cs
@@ -12,52 +12,46 @@ namespace SurgeEngine.Code.Gameplay.CommonObjects.Mobility
     public class JumpCollision : ContactBase
     {
         [Header("Properties")] 
-        [SerializeField] private float speedRequired = 20f;
+        [SerializeField] private float speedMin = 20f;
         [SerializeField, Range(0, 90)] private float pitch = 10f;
+        [SerializeField] private bool groundOnly = true;
         [SerializeField, Min(0)] private float impulseOnNormal = 15f;
         [SerializeField, Min(0)] private float impulseOnBoost = 15f;
         [SerializeField] private float outOfControl = 0.5f;
-
-        private BoxCollider _box;
 
         public override void Contact(Collider msg, ActorBase context)
         {
             base.Contact(msg, context);
             
-            float dot = Vector3.Dot(context.transform.forward, context.transform.forward);
+            float dot = Vector3.Dot(context.transform.forward, transform.forward);
             float impulse = SonicTools.IsBoost() ? impulseOnBoost : impulseOnNormal;
             
             if (dot > 0) // Make sure the player is facing the same direction as the jump collision
             {
-                if (context.Kinematics.Speed >= speedRequired) // If the player is moving fast enough apply an impulse
+                if (context.Kinematics.Speed >= speedMin)
                 {
                     if (impulse > 0)
                     {
-                        Rigidbody body = context.Kinematics.Rigidbody;
-                        body.linearVelocity = Vector3.zero;
-
-                        context.transform.position += Vector3.up * 0.25f;
-
-                        Vector3 impulseV = transform.forward * impulse;
-                        
-                        body.AddForce(impulseV, ForceMode.Impulse);
-                        body.linearVelocity = Vector3.ClampMagnitude(body.linearVelocity, impulse);
+                        var ray = new Ray(context.transform.position, -context.Kinematics.Normal);
+                        if (groundOnly && Physics.SphereCast(ray, 0.5f, out _, context.Config.castDistance, context.Config.castLayer) || !groundOnly)
+                        {
+                            Rigidbody body = context.Kinematics.Rigidbody;
+                            body.position += Vector3.up * 0.2f;
+                            
+                            Vector3 force = Utility.GetImpulseWithPitch(transform.forward, -transform.right, pitch, impulse);
+                            context.Kinematics.ResetVelocity();
+                            body.AddForce(force, ForceMode.Impulse);
+                            body.linearVelocity = Vector3.ClampMagnitude(body.linearVelocity, impulse);
+                            
+                            context.Flags.AddFlag(new Flag(FlagType.OutOfControl, true, outOfControl));
+                        }
                     }
-                
-                    context.Flags.AddFlag(new Flag(FlagType.OutOfControl, true, outOfControl));
                 }
             }
         }
 
         private void OnDrawGizmosSelected()
         {
-            if (_box == null)
-                _box = GetComponent<BoxCollider>();
-            
-            Gizmos.color = new Color(0f, 1f, 0.33f, 0.16f);
-            Gizmos.matrix = transform.localToWorldMatrix;
-            Gizmos.DrawCube(_box.center, _box.size);
-            
             TrajectoryDrawer.DrawTrajectory(transform.position, Utility.GetImpulseWithPitch(transform.forward, -transform.right, pitch, impulseOnNormal), Color.green, impulseOnNormal);
             TrajectoryDrawer.DrawTrajectory(transform.position, Utility.GetImpulseWithPitch(transform.forward, -transform.right, pitch, impulseOnBoost), Color.cyan, impulseOnBoost);
         }
