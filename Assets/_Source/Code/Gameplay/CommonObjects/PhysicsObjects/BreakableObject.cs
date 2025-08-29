70b8fdf20d14ca82b31f91d86a996c4a4956bc74
sokilo <sokilopersonal@gmail.com>
2025-08-29 12:44:33 +0300

    Added proper sound for Potbig breaking

--- Assets/_Source/Code/Gameplay/CommonObjects/PhysicsObjects/BreakableObject.cs
+++ Assets/_Source/Code/Gameplay/CommonObjects/PhysicsObjects/BreakableObject.cs
@@ -1,23 +1,49 @@
-﻿using FMODUnity;
+﻿using System;
+using FMODUnity;
 using SurgeEngine.Code.Gameplay.CommonObjects.Interfaces;
 using UnityEngine;
 
 namespace SurgeEngine.Code.Gameplay.CommonObjects.PhysicsObjects
 {
-    [SelectionBase]
+    [SelectionBase, RequireComponent(typeof(Rigidbody))]
     public class BreakableObject : MonoBehaviour, IDamageable
     {
         [Header("Destroy")]
         [SerializeField] protected DestroyedPiece destroyPiece;
         [SerializeField] protected EventReference destroyEvent;
+        
+        private Rigidbody _rigidbody;
+        private bool _destroyed;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
 
         public virtual void TakeDamage(Component sender)
         {
-            var piece = Instantiate(destroyPiece, transform.position, transform.rotation, null);
-            piece.ApplyDirectionForce(sender.GetComponentInChildren<Rigidbody>().linearVelocity, 1.1f);
+            Destroy(sender.GetComponentInChildren<Rigidbody>().linearVelocity);
+        }
+
+        private void Destroy(Vector3 piecesForce)
+        {
+            if (!_destroyed)
+            {
+                var piece = Instantiate(destroyPiece, transform.position, transform.rotation, null);
+                piece.ApplyDirectionForce(piecesForce, 1.1f);
             
-            RuntimeManager.PlayOneShot(destroyEvent, transform.position);
-            Destroy(gameObject);
+                RuntimeManager.PlayOneShot(destroyEvent, transform.position);
+                Destroy(gameObject);
+                _destroyed = true;
+            }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (_rigidbody.linearVelocity.magnitude > 15f)
+            {
+                Destroy(_rigidbody.linearVelocity);
+            }
         }
     }
 }
