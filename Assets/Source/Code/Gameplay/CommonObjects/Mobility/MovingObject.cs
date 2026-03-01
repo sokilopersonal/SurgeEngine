1f49326a63d0952046a3af37084ca5a0da7d1306
sokilouma <sokilopersonal@gmail.com>
2026-03-01 17:04:08 +0200

    Moving Object lagging fix

--- Assets/Source/Code/Gameplay/CommonObjects/Mobility/MovingObject.cs
+++ Assets/Source/Code/Gameplay/CommonObjects/Mobility/MovingObject.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 namespace SurgeEngine.Source.Code.Gameplay.CommonObjects.Mobility
 {
+    [DefaultExecutionOrder(1)] // Fix character lagging behind the actual object movement
     public class MovingObject : MonoBehaviour, IPointMarkerLoader
     {
         private List<Rigidbody> _bodies;
         private Vector3 _lastPosition;
         private Vector3 _velocity;
+        private Quaternion _lastRotation;
+        private Quaternion _deltaRotation;
 
         public event Action<Rigidbody> OnBodyAdded; 
 
@@ -17,17 +20,26 @@ namespace SurgeEngine.Source.Code.Gameplay.CommonObjects.Mobility
         {
             _bodies = new List<Rigidbody>();
             _lastPosition = transform.position;
+            _lastRotation = transform.rotation;
         }
 
         private void FixedUpdate()
         {
             _velocity = (transform.position - _lastPosition) / Time.fixedDeltaTime;
+            _deltaRotation = transform.rotation * Quaternion.Inverse(_lastRotation);
             _lastPosition = transform.position;
+            _lastRotation = transform.rotation;
 
             if (_bodies.Count > 0)
             {
-                foreach (var body in _bodies)
-                    body.MovePosition(body.position + _velocity * Time.fixedDeltaTime);
+                for (int i = 0; i < _bodies.Count; i++)
+                {
+                    var body = _bodies[i];
+                    body.position += _velocity * Time.fixedDeltaTime;
+                    Vector3 offset = body.position - transform.position;
+                    body.position = transform.position + _deltaRotation * offset;
+                    body.rotation = _deltaRotation * body.rotation;
+                }
             }
         }
 
